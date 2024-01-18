e8b8184fe2a01e41c3f8cd2d4adf3d86128febe5
Sasibru <sander.simonsen.brustad@gmail.com>
2024-01-18 10:15:48 +0100

    Add Controller

---
+++ BorroApp/Controller/BorroController.cs
@@ -0,0 +1,19 @@
+﻿using BorroApp.Data.Models;
+using BorroApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BorroApp.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BorroController : ControllerBase
+    {
+        private readonly BorroDbContext _context;
+        public BorroController(BorroDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
