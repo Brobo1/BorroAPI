7ca0fc3351a128c4d77557bcf06a103331038034
Sasibru <sander.simonsen.brustad@gmail.com>
2024-01-18 10:28:30 +0100

    Add GetAllPosts, GetOnePost and CreatePost

--- BorroApp/Controller/BorroController.cs
+++ BorroApp/Controller/BorroController.cs
@@ -15,5 +15,47 @@ namespace BorroApp.Controller
             _context = context;
         }
 
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPosts()
+        {
+            return Ok(await _context.Post.ToListAsync());
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOnePost(int id)
+        {
+            var post = await _context.Post.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(Post createPost)
+        {
+            var newPost = new Post
+            {
+                Title = createPost.Title,
+                Image = createPost.Image,
+                Price = createPost.Price,
+                DateFrom = createPost.DateFrom,
+                DateTo = createPost.DateTo,
+                Description = createPost.Description,
+                Location = createPost.Location,
+                CategoryId = createPost.CategoryId,
+                UserId = createPost.UserId,
+            };
+
+            _context.Post.Add(newPost);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetOnePost), new { id = newPost.Id }, newPost);
+        }
+
     }
 }
