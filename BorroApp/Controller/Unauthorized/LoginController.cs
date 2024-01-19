78cb4a15b12664c1bda2535a65b2f3455a186c7c
Sasibru <sander.simonsen.brustad@gmail.com>
2024-01-19 09:17:15 +0100

    Created subfolders "Authorized" and "Unauthorized"

--- BorroApp/Controller/Unauthorized/LoginController.cs
+++ BorroApp/Controller/Unauthorized/LoginController.cs
@@ -5,7 +5,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 
-namespace BorroApp.Controller
+namespace BorroApp.Controller.Unauthorized
 {
     [Route("api/[controller]")]
     [ApiController]
