8e201a483439b7d3139e8794a220cde4c42cd462
Sasibru <sander.simonsen.brustad@gmail.com>
2024-01-24 13:19:15 +0100

    Add apiController in FileUpload

--- BorroApp/Controller/FileUploadController.cs
+++ BorroApp/Controller/FileUploadController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BorroApp.Controller
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class FileUploadController : ControllerBase
     {
         private readonly BorroDbContext _context;
